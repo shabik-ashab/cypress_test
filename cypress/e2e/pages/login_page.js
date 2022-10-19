02981174062944337c74311759be1f61e490834d
nub69 <shabik.ashab@gmail.com>
2022-10-19 11:54:49 +0600

    created class for login

---
+++ cypress/e2e/pages/login_page.js
@@ -0,0 +1,17 @@
+
+export class loginPage{
+
+    enterUserName(){
+        cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+    }
+    enterPassword(){
+        cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+    }
+    clickLogin(){
+        cy.get('.oxd-button').click()
+
+    }
+
+}
---
+++ cypress/e2e/pom.cy.js
@@ -0,0 +1,12 @@
+
+
+it('pom',()=> {
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+    cy.get('.oxd-button').click()
+
+})
