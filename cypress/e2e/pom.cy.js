c833d0036811e6d160264c2f85d81639ef02d812
nub69 <shabik.ashab@gmail.com>
2022-10-19 13:34:25 +0600

    multiple test under one spec

--- cypress/e2e/pom.cy.js
+++ cypress/e2e/pom.cy.js
@@ -18,3 +18,19 @@ it('pom',()=> {
     // cy.get('.oxd-button').click()
 
 })
+
+it('login test2',()=> {
+
+    cy.visit("https://opensource-demo.orangehrmlive.com/")
+
+    loginPage.enterUserName('Admin122');
+    loginPage.enterPassword('admin123');
+    loginPage.clickLogin();
+
+    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+
+    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+
+    // cy.get('.oxd-button').click()
+
+})
