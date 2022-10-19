5fba6355c5cf1ba8b61c83a9dce1fa947ede3452
nub69 <shabik.ashab@gmail.com>
2022-10-19 11:28:04 +0600

    remove node_modules

--- cypress/e2e/assertion.cy.js
+++ cypress/e2e/assertion.cy.js
@@ -1,19 +1,29 @@
 
+
+
 it('assertion', ()=> {
     cy.visit('https://example.cypress.io')
 
+    // implicit assertion
     cy.contains('get').click()
 
     cy.get('#query-btn')
     .should('contain','Button')
-    .should('have.class', 'query-btn')
+    .and('have.class', 'query-btn')
+    .and('be.visible')
+    .and('be.enabled')
 
-    // should contain,have,be
+    // should is a asertion
+    // if we use and it will follow prev assertion
+    // should contain,have,be,equal
 
     // we can also check
     // have.text
     // have.html
 
-    // 
+    // be.visible, selected, disabled, focused = have.focous
+
+    // chaining assertion with add
+
 
 })
