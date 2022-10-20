a994d92493e287d9a00b9457a057602915fab0ce
nub69 <shabik.ashab@gmail.com>
2022-10-20 10:46:07 +0600

    forEach and describe block

--- cypress/e2e/pom.cy.js
+++ cypress/e2e/pom.cy.js
@@ -3,34 +3,33 @@ import { LoginPage } from './pages/login_page';
 
 const loginPage = new LoginPage();
 
-it('pom',()=> {
+describe('all login test',()=>{
 
-    cy.visit("https://opensource-demo.orangehrmlive.com/")
+    beforeEach(()=> {
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+    })
 
-    loginPage.enterUserName('Admin');
-    loginPage.enterPassword('admin123');
-    loginPage.clickLogin();
+    it.only('pom',()=> {
 
-    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-
-    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-
-    // cy.get('.oxd-button').click()
-
-})
-
-it('login test2',()=> {
-
-    cy.visit("https://opensource-demo.orangehrmlive.com/")
-
-    loginPage.enterUserName('Admin122');
-    loginPage.enterPassword('admin123');
-    loginPage.clickLogin();
-
-    // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
-
-    // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
-
-    // cy.get('.oxd-button').click()
+        loginPage.enterUserName('Admin');
+        loginPage.enterPassword('admin123');
+        loginPage.clickLogin();
+    
+        // cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('Admin')
+    
+        // cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin123')
+    
+        // cy.get('.oxd-button').click()
+    
+    })
+    
+    it('login test2',()=> {
+    
+        loginPage.enterUserName('Admin122');
+        loginPage.enterPassword('admin123');
+        loginPage.clickLogin();
+    
+    })
 
 })
+
